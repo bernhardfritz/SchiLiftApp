7665700ea764fe100dcad06d5cde5f9c3bcd50b4
Bernhard Fritz <bernhardfritz@Bernhards-MacBook-Pro.local>
2015-01-16 20:19:03 +0100

    view update

--- play-java/app/controllers/Tool.java
+++ play-java/app/controllers/Tool.java
@@ -2,7 +2,7 @@ package controllers;
 
 public class Tool {
 	public static int StringToInt(String s) {
-		return s.equals("")?0:Integer.parseInt(s.replace(".", " ").replace(" ", ""));
+		return s.equals("")?0:Integer.parseInt(s.replace(".0", "").replace(".", " ").replace(" ", ""));
 	}
 	
 	public static float StringToFloat(String s) {
