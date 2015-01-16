5000d8191025357d177bfeff51580a69970bc993
philipphaeusle <philipp.haeusle@hotmail.com>
2015-01-16 10:52:35 +0100

    calculte lef Time added, and HashManager in a new class

---
+++ play-java/app/controllers/HashManager.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.commons.codec.binary.Hex;
+
+
+public class HashManager {
+	private static MessageDigest algorythm;
+	
+	public HashManager() { // creates a MessageDigest with MD5
+		try {
+			algorythm = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException nsae) {
+			nsae.printStackTrace();
+		}	
+		
+	}
+	public static String codeString(String x) { //encodes the given String with MessageDigest
+		algorythm.update(x.getBytes());
+		byte[] digest = algorythm.digest();
+		String output = Hex.encodeHexString(digest);
+		return output;
+}
+}
