8e1d8f6dc41b638f17ec90ca06b6d8cb8339cb96
Bernhard Fritz <bernhardfritz@Bernhards-MacBook-Pro.local>
2015-01-16 19:57:45 +0100

    view update

--- play-java/app/controllers/Application.java
+++ play-java/app/controllers/Application.java
@@ -11,8 +11,17 @@ public class Application extends Controller {
         return ok(index.render(""));
     }
     
+    public static Result suche() {
+    	DBManager dbman = DBManager.getInstance();
+    	return ok(suche.render("",dbman.getAlleSchigebiete()));
+    }
+    
     public static Result suche(String suchbegriff) {
-    	return ok(suche.render(suchbegriff,null));
+    	if(suchbegriff.equals("")) return suche();
+    	else {
+    		DBManager dbman = DBManager.getInstance();
+    		return ok(suche.render(suchbegriff,dbman.getSchigebiete(suchbegriff)));
+    	}
     }
     
     public static Result schigebiet(String gemeinde) {
