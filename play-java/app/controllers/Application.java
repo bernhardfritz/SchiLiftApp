ea82e244eca8c278eabc95919c68d3e8ea223c05
Bernhard Fritz <bernhardfritz@Bernhards-MacBook-Pro.local>
2015-01-16 18:43:22 +0100

    view update

--- play-java/app/controllers/Application.java
+++ play-java/app/controllers/Application.java
@@ -8,7 +8,21 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render(""));
+    }
+    
+    public static Result suche(String suchbegriff) {
+    	return ok(suche.render(suchbegriff));
+    }
+    
+    public static Result schigebiet(String gemeinde) {
+    	DBManager dbman = DBManager.getInstance();
+    	return ok(schigebiet.render(gemeinde,dbman.getSchigebiet(gemeinde).getSchilifte()));
+    }
+    
+    public static Result schilift(String id) {
+    	DBManager dbman = DBManager.getInstance();
+    	return ok(schilift.render(null));
     }
 
 }
