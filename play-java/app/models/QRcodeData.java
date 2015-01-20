24fe8a9c8abd0e9560c982f303956abd7650e97a
Bernhard Fritz <bernhardfritz@Bernhards-MacBook-Pro.local>
2015-01-20 16:41:37 +0100

    view update

--- play-java/app/models/QRcodeData.java
+++ play-java/app/models/QRcodeData.java
@@ -49,29 +49,6 @@ public class QRcodeData extends Model{
 		this.HashCode=this.toHash();
 	}
 	
-	/*public QRcodeData(String data){ //Constructor for a String (!! 2015/1/1 12:01 muss als 201501011201 geschrieben werden!!)
-		char[] dataarray= data.toCharArray(); 
-		this.userID=Character.getNumericValue(dataarray[0])*1000+Character.getNumericValue(dataarray[1])*100+Character.getNumericValue(dataarray[2])*10+Character.getNumericValue(dataarray[3]);
-		Integer StartYear=Character.getNumericValue(dataarray[4])*1000+Character.getNumericValue(dataarray[5])*100+Character.getNumericValue(dataarray[6])*10+Character.getNumericValue(dataarray[7]);
-		Integer StartMonth= Character.getNumericValue(dataarray[8])*10+Character.getNumericValue(dataarray[9]);
-		Integer StartDay=Character.getNumericValue(dataarray[10])*10+Character.getNumericValue(dataarray[11]);
-		Integer StartHour=Character.getNumericValue(dataarray[12])*10+Character.getNumericValue(dataarray[13]);
-		Integer StartMinute=Character.getNumericValue(dataarray[14])*10+Character.getNumericValue(dataarray[15]);
-		Integer EndYear=Character.getNumericValue(dataarray[16])*1000+Character.getNumericValue(dataarray[17])*100+Character.getNumericValue(dataarray[18])*10+Character.getNumericValue(dataarray[19]);
-		Integer EndMonth= Character.getNumericValue(dataarray[20])*10+Character.getNumericValue(dataarray[21]);
-		Integer EndDay=Character.getNumericValue(dataarray[22])*10+Character.getNumericValue(dataarray[23]);
-		Integer EndHour=Character.getNumericValue(dataarray[24])*10+Character.getNumericValue(dataarray[25]);
-		Integer EndMinute=Character.getNumericValue(dataarray[26])*10+Character.getNumericValue(dataarray[27]);
-		this.Start = new LocalDateTime(StartYear, StartMonth, StartDay,StartHour, StartMinute);
-		this.End = new LocalDateTime(EndYear, EndMonth, EndDay, EndHour,EndMinute);
-		String Gemeinde="";
-		for (int i=28; i<dataarray.length; i++){
-			Gemeinde+=dataarray[i];
-		}
-		this.Gemeinde= Gemeinde;
-		this.HashCode=this.toHash();
-	}*/
-	
 	public QRcodeData(Integer userID,String startdatetime, String enddatetime,String gemeinde) { // zb. 01/14/2015 11:21 AM
 		this.userID=userID;
 	    Start = LocalDateTime.parse(startdatetime, DateTimeFormat.forPattern("MM/dd/yyyy KK:mm aa"));
@@ -119,26 +96,15 @@ public class QRcodeData extends Model{
 	}
 
 	public String leftTime() { // returns String with left Time from now until end
-		String left = null;
-		if (isValid()){
-		if (getLeftDays()>0){
-		left = "Verbliebene Zeit - \nTage: " + getLeftDays().toString() + "   Stunden: "
-				+ getLeftHours().toString() + "   Minuten: "
-				+ getLeftMinutes().toString();
-		}else{
-			if (getLeftHours()>0){
-			left =  "Verbliebene Zeit\nStunden: "
-					+ getLeftHours().toString() + "   Minuten: "
-					+ getLeftMinutes().toString();
-			}else{
-				left =  "Verbliebene Zeit\nMinuten: "
-						+ getLeftMinutes().toString();
-				}
-			}
-		return left;
-		}else{
-			return "Nicht Gültig";
-		}
+		if(!isValid()) return "QR-Code ist abgelaufen.";
+		StringBuilder sb = new StringBuilder();
+		sb.append("Noch ");
+		if(getLeftDays()>0) sb.append(getLeftDays()).append(" Tage "); 
+		if(getLeftHours()>0) sb.append(getLeftHours()).append(" Stunden ");
+		if(getLeftMinutes()>0) sb.append(getLeftMinutes()).append(" Minuten ");
+		else sb.append("weniger als eine Minute ");
+		sb.append("gültig.");
+		return sb.toString();
 	}
 		
 	
